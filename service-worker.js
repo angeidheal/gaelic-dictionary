75651b49bbd75c8d970657100803bcc8a975e35f
Chris Mitchell <16734687+angeidheal@users.noreply.github.com>
2025-03-15 19:46:52 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
- 
+const CACHE_NAME = 'gaelic-dictionary-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/logo.png',
+  '/logo192.png',
+  '/logo512.png',
+  '/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
